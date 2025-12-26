a189c994d7e5b8ab82ea2e28cfd76e195f51b93d
linnea <linnea.jklaarup@gmail.com>
2025-12-26 17:40:04 +0100

    test button

--- dist/commands/commands.js
+++ dist/commands/commands.js
@@ -1,40 +1,54 @@
-/* global Office */
 alert("commands.js loaded");
 
+/* global Office */
 Office.onReady(() => {});
 
-// This function will be triggered by the ribbon button
 async function generateBirthdays(event) {
-    try {
-        const token = await OfficeRuntime.auth.getAccessToken({
-            allowSignInPrompt: true,
-            allowConsentPrompt: true
-        });
-
-        const year = new Date().getFullYear();
-
-        const response = await fetch(
-            "https://birthdaysync.azurewebsites.net/api/generate-birthdays",
-            {
-                method: "POST",
-                headers: {
-                    "Authorization": `Bearer ${token}`,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ year })
-            }
-        );
-
-        if (!response.ok) throw new Error("Azure Function call failed");
-
-        console.log("Birthday events generated successfully");
-    } catch (err) {
-        console.error(err);
-    } finally {
-        // Notify Outlook the action finished
-        event.completed();
-    }
+    alert("Generate button clicked");
+    event.completed();
 }
 
-// Make the function globally available
-Office.actions.associate("generateBirthdays", generateBirthdays);
+window.generateBirthdays = generateBirthdays;
+
+
+// /* global Office */
+
+// alert("commands.js loaded");
+//
+// Office.onReady(() => {});
+//
+// // This function will be triggered by the ribbon button
+// async function generateBirthdays(event) {
+//     try {
+//         const token = await OfficeRuntime.auth.getAccessToken({
+//             allowSignInPrompt: true,
+//             allowConsentPrompt: true
+//         });
+//
+//         const year = new Date().getFullYear();
+//
+//         const response = await fetch(
+//             "https://birthdaysync.azurewebsites.net/api/generate-birthdays",
+//             {
+//                 method: "POST",
+//                 headers: {
+//                     "Authorization": `Bearer ${token}`,
+//                     "Content-Type": "application/json"
+//                 },
+//                 body: JSON.stringify({ year })
+//             }
+//         );
+//
+//         if (!response.ok) throw new Error("Azure Function call failed");
+//
+//         console.log("Birthday events generated successfully");
+//     } catch (err) {
+//         console.error(err);
+//     } finally {
+//         // Notify Outlook the action finished
+//         event.completed();
+//     }
+// }
+//
+// // Make the function globally available
+// Office.actions.associate("generateBirthdays", generateBirthdays);
